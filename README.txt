317fedbb5300d5972fded83cf3ab704b8d381128
infancy <ray.tracing@qq.com>
2023-12-30 16:11:31 +0800

    arhosek12_spectrum_sky

---
+++ README.txt
@@ -0,0 +1,11 @@
+
+# reference
+
+* Charles University Computer Graphics Group - Sky Dome Research https://cgg.mff.cuni.cz/projects/SkylightModelling/
+
+* githole/Skydome: An Analytic Model for Full Spectral Sky-Dome Radiance https://github.com/githole/Skydome
+:
+
+* Pete Shirley's Graphics Blog: Converting Spectra to XYZ/RGB values https://psgraphics.blogspot.com/2014/11/converting-spectra-to-xyzrgb-values.html
+
+* Simple Analytic Approximationsto the CIE XYZ Color Matching Functions (JCGT) https://jcgt.org/published/0002/02/01/
--- skymodely.cpp
+++ skymodely.cpp
@@ -38,25 +38,90 @@ struct color_t
     }
 };
 
-struct spectrum_t
+template<int wavelength_start_, int samples_, int delta_>
+class base_spectrum_t
 {
-    // TODO
+public:
+    static constexpr int k_wavelength_start{ wavelength_start_ };
+    static constexpr int k_wavelength_end{ wavelength_start_ + samples_ * delta_ };
 
-    void to_rgb()
+    static constexpr int k_samples{ samples_ };
+    static constexpr int k_delta{ delta_ };
+
+public:
+    float& operator[](int i) { return a_[i]; }
+    float wavelength(int i) { return k_wavelength_start + i * delta_; }
+
+    color_t to_srgb()
     {
+        color_t color{};
+
+        for (int i = 0; i < k_samples; ++i)
+        {
+            auto& spectrum = a_;
+            float wavelen = wavelength(i);
+
+            color.x += spectrum[i] * xFit_1931(wavelen) * k_delta;
+            color.y += spectrum[i] * yFit_1931(wavelen) * k_delta;
+            color.z += spectrum[i] * zFit_1931(wavelen) * k_delta;
+        }
+
+        return color.xyz_to_srgb();
     }
+
+private:
+    std::array<float, k_samples> a_{};
+
+private:
+    // Simple Analytic Approximationsto the CIE XYZ Color Matching Functions (JCGT) https://jcgt.org/published/0002/02/01/
+    float xFit_1931(float wavelen)
+    {
+        float t1 = (wavelen - 442.0f) * ((wavelen < 442.0f) ? 0.0624f : 0.0374f);
+        float t2 = (wavelen - 599.8f) * ((wavelen < 599.8f) ? 0.0264f : 0.0323f);
+        float t3 = (wavelen - 501.1f) * ((wavelen < 501.1f) ? 0.0490f : 0.0382f);
+        return 0.362f * expf(-0.5f * t1 * t1) + 1.056f * expf(-0.5f * t2 * t2) - 0.065f * expf(-0.5f * t3 * t3);
+    }
+
+    float yFit_1931(float wavelen)
+    {
+        float t1 = (wavelen - 568.8f) * ((wavelen < 568.8f) ? 0.0213f : 0.0247f);
+        float t2 = (wavelen - 530.9f) * ((wavelen < 530.9f) ? 0.0613f : 0.0322f);
+        return 0.821f * exp(-0.5f * t1 * t1) + 0.286f * expf(-0.5f * t2 * t2);
+    }
+
+    float zFit_1931(float wavelen)
+    {
+        float t1 = (wavelen - 437.0f) * ((wavelen < 437.0f) ? 0.0845f : 0.0278f);
+        float t2 = (wavelen - 459.0f) * ((wavelen < 459.0f) ? 0.0385f : 0.0725f);
+        return 1.217f * exp(-0.5f * t1 * t1) + 0.681f * expf(-0.5f * t2 * t2);
+    }
+};
+
+using spectrum300_t = base_spectrum_t<400, 300,  1>;
+using spectrum60_t  = base_spectrum_t<400,  60,  5>;
+using spectrum30_t  = base_spectrum_t<400,  30, 10>;
+using spectrum10_t  = base_spectrum_t<320,  10, 40>;
+
+enum class spectrum_samples_enum_t
+{
+    n300, // 400nm~700nm, 300 sample,  1nm per sample
+    n60,  // 400nm~700nm,  60 sample,  5nm per sample
+    n30,  // 400nm~700nm,  30 sample, 10nm per sample
+    n10,  // 320nm~720nm,  10 sample, 40nm per sample
+    count
 };
 
 
 enum class skymodel_enum_t
 {
     none,
-    arhosek12_rgb,
-    arhosek12_xyz,
-    arhosek12_spectrum_sky,
-    arhosek12_spectrum_sun,
-    arhosek12_spectrum_skysun,
+    arhosek12_rgb = 1,
+    arhosek12_xyz = 2,
+    arhosek12_spectrum_sky = 4,
+    arhosek12_spectrum_sun = 8,
+    arhosek12_spectrum_skysun = arhosek12_spectrum_sky | arhosek12_spectrum_sun,
 };
+bool enum_have(skymodel_enum_t skymodel_enum, skymodel_enum_t target) { return (int)skymodel_enum & (int)target; }
 
 class skymodel_t
 {
@@ -93,7 +158,7 @@ public:
 
     ~arhosek12_tristim_t()
     {
-        for (int i = 0; i < k_channels; i++)
+        for (int i = 0; i < k_channels; ++i)
         {
             arhosekskymodelstate_free(skymodel_state_[i]);
         }
@@ -101,9 +166,9 @@ public:
 
     color_t radiance(double theta, double gamma) override
     {
-        color_t color;
+        color_t color{};
 
-        for (int i = 0; i < k_channels; i++)
+        for (int i = 0; i < k_channels; ++i)
         {
             color[i] = arhosek_tristim_skymodel_radiance(
                 skymodel_state_[i],
@@ -123,11 +188,69 @@ public:
 private:
     static constexpr int k_channels = 3;
     skymodel_enum_t skymodel_enum_{};
-    ArHosekSkyModelState* skymodel_state_[k_channels]{};
+    std::array<ArHosekSkyModelState*, k_channels> skymodel_state_{};
+};
+
+template<typename spectrum_t>
+//    requires(  )
+class arhosek12_spectrum_t : public skymodel_t
+{
+public:
+    arhosek12_spectrum_t(
+        skymodel_enum_t skymodel_enum,
+        double turbidity,
+        color_t albedo,
+        double solar_elevation)
+    {
+        skymodel_enum_ = skymodel_enum;
+
+        // TODO: rgb to spectrum
+        spectrum_t spectrum_albedo{};
+
+        for (int i = 0; i < k_channels; ++i)
+        {
+            skymodel_state_[i] = arhosekskymodelstate_alloc_init(
+                solar_elevation,
+                turbidity,
+                spectrum_albedo[i]);
+        }
+    }
+
+    ~arhosek12_spectrum_t()
+    {
+        for (int i = 0; i < k_channels; ++i)
+        {
+            arhosekskymodelstate_free(skymodel_state_[i]);
+        }
+    }
+
+    color_t radiance(double theta, double gamma) override
+    {
+        spectrum_t spectrum{};
+
+        for (int i = 0; i < k_channels; ++i)
+        {
+            double wavelength = spectrum.wavelength(i);
+
+            if (enum_have(skymodel_enum_, skymodel_enum_t::arhosek12_spectrum_sky))
+                spectrum[i] += arhosekskymodel_radiance(skymodel_state_[i], theta, gamma, wavelength);
+
+            if (enum_have(skymodel_enum_, skymodel_enum_t::arhosek12_spectrum_sun))
+                spectrum[i] += arhosekskymodel_solar_radiance(skymodel_state_[i], theta, gamma, wavelength);
+        }
+
+        return spectrum.to_srgb();
+    }
+
+private:
+    static constexpr int k_channels{ spectrum_t::k_samples };
+    skymodel_enum_t skymodel_enum_{};
+    std::array<ArHosekSkyModelState*, k_channels> skymodel_state_{};
 };
 
 std::unique_ptr<skymodel_t> create_skymodel(
     skymodel_enum_t skymodel_enum,
+    spectrum_samples_enum_t spectrum_samples_eum,
     double turbidity,
     color_t albedo,
     double solar_elevation)
@@ -137,6 +260,20 @@ std::unique_ptr<skymodel_t> create_skymodel(
     case skymodel_enum_t::arhosek12_rgb:
     case skymodel_enum_t::arhosek12_xyz:
         return std::make_unique<arhosek12_tristim_t>(skymodel_enum, turbidity, albedo, solar_elevation);
+    case skymodel_enum_t::arhosek12_spectrum_sky:
+    case skymodel_enum_t::arhosek12_spectrum_sun:
+    case skymodel_enum_t::arhosek12_spectrum_skysun:
+    {
+        switch (spectrum_samples_eum)
+        {
+        case spectrum_samples_enum_t::n300:
+            return std::make_unique<arhosek12_spectrum_t<spectrum300_t>>(skymodel_enum, turbidity, albedo, solar_elevation);
+        case spectrum_samples_enum_t::n60:
+            return std::make_unique<arhosek12_spectrum_t<spectrum60_t>>(skymodel_enum, turbidity, albedo, solar_elevation);
+        case spectrum_samples_enum_t::n30:
+            return std::make_unique<arhosek12_spectrum_t<spectrum30_t>>(skymodel_enum, turbidity, albedo, solar_elevation);
+        }
+    }
     default:
         std::print("ERROR! no skymodel");
         return nullptr;
@@ -144,7 +281,7 @@ std::unique_ptr<skymodel_t> create_skymodel(
 }
 
 
-// e.g. skymodely filename width height turbidity(1~10) albedo(RGB) solar_elevation(degree, 0~180) skymodel_enum
+// e.g. skymodely filename width height turbidity(1~10) albedo(RGB) solar_elevation(degree, 0~180) skymodel_enum spectrum_samples_enum
 int main(int argc, char **argv)
 {
     using namespace std;
@@ -155,9 +292,10 @@ int main(int argc, char **argv)
     double turbidity = 1; // 1~10
     color_t albedo{ 0, 0, 0 };
     double solar_elevation = 90.0 / 180.0 * k_pi; // 0~180
-    skymodel_enum_t skymodel_enum = skymodel_enum_t::arhosek12_xyz;
+    skymodel_enum_t skymodel_enum = skymodel_enum_t::arhosek12_spectrum_sky;
+    spectrum_samples_enum_t spectrum_samples_eum = spectrum_samples_enum_t::n30;
 
-    if (argc == 10)
+    if (argc == 11)
     {
         filename = argv[1];
         width = atof(argv[2]);
@@ -166,11 +304,12 @@ int main(int argc, char **argv)
         albedo = color_t{ stof(argv[5]), stof(argv[6]), stof(argv[7]) };
         solar_elevation = atof(argv[8]) / 180.0 * k_pi;
         skymodel_enum = skymodel_enum_t(atoi(argv[9]));
+        spectrum_samples_eum = spectrum_samples_enum_t(atoi(argv[10]));
     }
 
-    auto skymodel = create_skymodel(skymodel_enum, turbidity, albedo, solar_elevation);
+    auto skymodel = create_skymodel(skymodel_enum, spectrum_samples_eum, turbidity, albedo, solar_elevation);
 
-    unique_ptr<color_t[]> img = make_unique<color_t[]>(sizeof(color_t) * width * height);
+    unique_ptr<color_t[]> img = make_unique<color_t[]>(width * height);
 
     for (int y = 0; y < height; y++)
     {
